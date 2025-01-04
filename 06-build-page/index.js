8e060a6fe989afe9def05f7b466b0472ecf32b07
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-01-04 22:03:08 +0100

    refactor: separate one bundle function into three different functions (html, styles, overall)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,29 +2,9 @@ const fs = require('node:fs/promises');
 const path = require('node:path');
 const { createReadStream, createWriteStream } = require('node:fs');
 
-async function bundleProject(
-  htmlSource,
-  stylesSource,
-  destination,
-  components,
-) {
+async function bundleHtml(source, destination, components) {
   try {
-    await fs.mkdir(destination, { recursive: true });
-
-    const destinationContent = await fs.readdir(destinationPath, {
-      withFileTypes: true,
-    });
-
-    for (const content of destinationContent) {
-      const contentPath = path.join(destinationPath, content.name);
-      if (content.isFile()) {
-        await fs.unlink(contentPath);
-      } else if (content.isDirectory()) {
-        await fs.rm(contentPath);
-      }
-    }
-
-    const templateContent = await fs.readFile(htmlSource, 'utf-8');
+    const templateContent = await fs.readFile(source, 'utf-8');
     const indexFilePath = path.join(destination, 'index.html');
     const indexFileName = path.basename(indexFilePath);
 
@@ -52,16 +32,20 @@ async function bundleProject(
     process.stdout.write(
       `File ${indexFileName} has been successfully created in ${destination}.\n`,
     );
+  } catch (error) {
+    process.stdout.write(`Error: ${error.message}`);
+  }
+}
 
+async function bundleStyles(source, destination) {
+  try {
     const writePath = path.join(destination, 'style.css');
     const writeFileName = path.basename(writePath);
     const writeStream = createWriteStream(writePath, 'utf-8');
-    const sourceContent = await fs.readdir(stylesSource, {
-      withFileTypes: true,
-    });
+    const sourceContent = await fs.readdir(source, { withFileTypes: true });
 
     for (const content of sourceContent) {
-      const contentPath = path.join(stylesSource, content.name);
+      const contentPath = path.join(source, content.name);
       const contentExtension = path.extname(contentPath);
 
       if (content.isFile() && contentExtension === '.css') {
@@ -70,6 +54,9 @@ async function bundleProject(
         for await (const chunk of readStream) {
           writeStream.write(`${chunk}\n`);
         }
+        process.stdout.write(
+          `${contentPath} is successfully merged into ${writeFileName}\n`,
+        );
       }
     }
 
@@ -85,10 +72,38 @@ async function bundleProject(
   }
 }
 
+async function bundleProject(
+  htmlSource,
+  stylesSource,
+  destination,
+  components,
+) {
+  try {
+    await fs.mkdir(destination, { recursive: true });
+    const destinationContent = await fs.readdir(destination, {
+      withFileTypes: true,
+    });
+
+    for (const content of destinationContent) {
+      const contentPath = path.join(destination, content.name);
+      if (content.isFile()) {
+        await fs.unlink(contentPath);
+      } else if (content.isDirectory()) {
+        await fs.rm(contentPath, { recursive: true });
+      }
+    }
+
+    await bundleHtml(htmlSource, destination, components);
+    await bundleStyles(stylesSource, destination);
+  } catch (error) {
+    process.stdout.write(`Error: ${error.message}\n`);
+  }
+}
+
 const stylesSourcePath = path.join(__dirname, 'styles');
 const componentsPath = path.join(__dirname, 'components');
 const htmlSourcePath = path.join(__dirname, 'template.html');
-const destinationPath = path.join(__dirname, 'projects-dist');
+const destinationPath = path.join(__dirname, 'project-dist');
 
 bundleProject(
   htmlSourcePath,
